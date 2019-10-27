fdae347901194f3a8def054aa10bcf81905b157b
eric.ranstrom <eric.ranstrom@workday.com>
2019-10-26 20:15:00 -0700

    export function stub

---
+++ exportAppointments.ts
@@ -0,0 +1,11 @@
+// The initialize function must be run each time a new page is loaded
+Office.initialize = reason => {
+    console.log('Office init...' + reason);
+};
+
+// Add any ui-less function here
+function export(event) {
+    var buttonId = event.source.id;
+    console.log('export() called, buttonID: ' + buttonId);
+    complete();
+}
