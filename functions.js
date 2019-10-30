38b31e9b95127756d08a1916c1cc6540b67c460d
eric.ranstrom <eric.ranstrom@workday.com>
2019-10-29 23:28:33 -0700

    undefined

--- functions.js
+++ functions.js
@@ -143,7 +143,7 @@ Category.prototype = {
 function newEvent(categoryName, subject, starttime, endtime){
   var event = new Event(subject, starttime, endtime)
   var category = getCategory(name)
-  if (category === null) {
+  if (category === undefined) {
      category = new Category(categoryName)
      category.addEvent(event)
      CATEGORIES.push(category)
